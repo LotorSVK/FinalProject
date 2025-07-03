b9391c74a8317bfaa5dd5cd6d46a96bf420e9183
LotorSVK <edwinkatona@gmail.com>
2025-07-03 20:16:44 +0200

    init commit

--- src/main/java/TutorialPointTableBdd.java
+++ src/main/java/TutorialPointTableBdd.java
@@ -1,17 +1,48 @@
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TutorialPointTableBdd {
-    @Given("Uzivate sa nachadza na stranke {string}")
-    public void uzivateSaNachadzaNaStranke(String arg0) {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp(){
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
     }
 
-    @Then("meno na druhom riadku tabulky bude {string}")
-    public void menoNaDruhomRiadkuTabulkyBude(String arg0) {
+    @After
+    public void cleanUp() {
+        //driver.close();
+        driver.quit();
     }
 
-    @And("priezvisko na druhom riadku tabulky bude {string}")
-    public void priezviskoNaDruhomRiadkuTabulkyBude(String arg0) {
+
+
+    @Given("Uzivatel se nachazi na strance {string}")
+    public void uzivatelSeNachaziNaStrance(String url) {
+        driver.get(url);
+    }
+
+    @Then("jmeno na druhem radku tabulky bude {string}")
+    public void jmenoNaDruhemRadkuTabulkyBude(String jmeno) {
+        String name = driver.findElement(By.xpath("/html/body/main/div/div/div[2]/form/div[2]/table/tbody/tr[2]/td[1]")).getText();
+        assertEquals(jmeno, name);
+    }
+
+    @And("prijmeni na druhem radku tabulky bude {string}")
+    public void prijmeniNaDruhemRadkuTabulkyBude(String prijmeni) {
+        String lastName = driver.findElement(By.xpath("/html/body/main/div/div/div[2]/form/div[2]/table/tbody/tr[2]/td[2]")).getText();
+        assertEquals(prijmeni, lastName);
     }
 }
--- src/main/java/org/example/TutorialPointTable.java
+++ src/main/java/org/example/TutorialPointTable.java
@@ -38,6 +38,7 @@ public class TutorialPointTable {
         String salary = driver.findElement(By.xpath("/html/body/main/div/div/div[2]/form/div[2]/table/tbody/tr[2]/td[5]")).getText();
         String department = driver.findElement(By.xpath("/html/body/main/div/div/div[2]/form/div[2]/table/tbody/tr[2]/td[6]")).getText();
 
+
         assertEquals("Alden", name);
         assertEquals("Cantrell",lastName);
         assertEquals("45",age);
