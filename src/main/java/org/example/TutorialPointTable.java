e06ed491369e712f15894fd18cddde114d05d65c
LotorSVK <edwinkatona@gmail.com>
2025-07-03 19:25:08 +0200

    init commit

--- src/main/java/org/example/TutorialPointTable.java
+++ src/main/java/org/example/TutorialPointTable.java
@@ -2,8 +2,11 @@ package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TutorialPointTable {
 
@@ -13,6 +16,31 @@ public class TutorialPointTable {
     public void setUp(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+    }
+
+    @Test
+    public void test(){
+        driver.get("https://www.tutorialspoint.com/selenium/practice/webtables.php#");
+        String name = driver.findElement(By.xpath("/html/body/main/div/div/div[2]/form/div[2]/table/tbody/tr[2]/td[1]")).getText();
+        String lastName = driver.findElement(By.xpath("/html/body/main/div/div/div[2]/form/div[2]/table/tbody/tr[2]/td[2]")).getText();
+        String age = driver.findElement(By.xpath("/html/body/main/div/div/div[2]/form/div[2]/table/tbody/tr[2]/td[3]")).getText();
+        String email = driver.findElement(By.xpath("/html/body/main/div/div/div[2]/form/div[2]/table/tbody/tr[2]/td[4]")).getText();
+        String salary = driver.findElement(By.xpath("/html/body/main/div/div/div[2]/form/div[2]/table/tbody/tr[2]/td[5]")).getText();
+        String department = driver.findElement(By.xpath("/html/body/main/div/div/div[2]/form/div[2]/table/tbody/tr[2]/td[6]")).getText();
+
+
+        assertEquals("Alden", name);
+        assertEquals("Cantrell",lastName);
+        assertEquals("45",age);
+        assertEquals("alden@example.com",email);
+        assertEquals("12000",salary);
+        assertEquals("Compliance",department);
+
+
+
+
+
+
     }
 
 }
